ff278ab66b6e2b1b1dd354aadc53328c24cee450
emman-m <84948327+emman-m@users.noreply.github.com>
2024-03-11 11:11:29 +0000

    Delete other user in user_account table

--- database/db_visitlog.sql
+++ database/db_visitlog.sql
@@ -74,15 +74,7 @@ TRUNCATE TABLE `user_account`;
 --
 
 INSERT INTO `user_account` (`id`, `active`, `role`, `department`, `firstname`, `lastname`, `email`, `image`, `username`, `password`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 0, 'Admin', 'Account', 'admin@visitlog.com', '', 'admin', '$2y$10$x4SkweniwHr7x4vjQgkgCuYJziUI9GUw2cFniIxo11esZTgWlOXOa', '2024-01-15 04:20:53', '2024-02-21 19:23:51'),
-(3, 1, 3, 4, 'Deacon', 'Carney', 'selesirujo@mailinator.com', '', 'zytuk', '$2y$10$OF8IaolnGB8Zx9MQ4i7qo.UZhlibcV12CGYxAVByu4dlTWJ9ENQG2', '2024-01-18 11:57:57', '2024-03-07 17:23:41'),
-(5, 1, 3, 3, 'Angelica', 'Mcclain', 'pufikug@mailinator.com', '', 'cunoqiqyw', '$2y$10$LnZOMSJA22uJr9JG.KeK2uBvYbBY50wCNclUYz13ESehVKUyTf.Ga', '2024-01-19 06:05:19', '2024-03-02 10:46:53'),
-(6, 1, 3, 2, 'Medge', 'Mathis', 'gigisykavi@mailinator.com', '', 'fesamyfu', '$2y$10$MojfIbH.KjEl.JVJuyYnw.9kRC7BXljaeaCxxrutLGd1mFLaMeg6u', '2024-01-19 06:10:57', '2024-03-07 17:24:32'),
-(7, 1, 2, 0, 'Gretchen', 'Hodges', 'kykaqo@mailinator.com', '', 'kocelep', '$2y$10$SD43RBnaDeINqBSnLIjDM.L39igY7tpZFUj8kHRekCVzah1j.jYVC', '2024-01-19 06:11:17', '2024-03-09 15:42:56'),
-(8, 1, 3, 1, 'Neve', 'Haley', 'qitel@mailinator.com', '', 'qifanur', '$2y$10$uQwWta0.BTQBbMouEVUA5.VOg7pw94W6Bq/8xTGyUKLea7cq4Z1cS', '2024-01-20 05:54:04', '2024-03-04 18:10:33'),
-(9, 1, 2, 0, 'Quon', 'Schroeder', 'vunoti@mailinator.com', '', 'lopydod', '$2y$10$9oDVDe0x5R7EQ62VfyCxKeXk/DhQXFHWpPyeaFiaEoGgNwoOh7H/.', '2024-01-20 06:00:32', '2024-03-06 17:26:16'),
-(10, 0, 3, 1, 'Aphrodite', 'Orr', 'fovecom@mailinator.com', '', 'dolewy', '$2y$10$tO/R2lyN2ncm7vFftpcIaegkXpwohNEbYd7Wig.rHbPFr3SHplEkG', '2024-02-12 10:04:41', '2024-02-17 15:37:46'),
-(11, 1, 3, 5, 'Nissim', 'Mccormick', 'hyxicali@mailinator.com', '', 'guard', '$2y$10$EYqrdPTSQCJyn.6dc6RigObzkz0GbnhAlv4wXo11gHskFXMI/8hee', '2024-02-14 11:45:02', '2024-03-04 18:10:41');
+(1, 1, 1, 0, 'Admin', 'Account', 'admin@visitlog.com', '', 'admin', '$2y$10$x4SkweniwHr7x4vjQgkgCuYJziUI9GUw2cFniIxo11esZTgWlOXOa', '2024-01-15 04:20:53', '2024-02-21 19:23:51');
 
 -- --------------------------------------------------------
 
